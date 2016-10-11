5603367d0889c6b0e984839cfc203d1b1f00ece9
akkarati <akkarat.i@hitachi-tstv.com>
2016-10-11 13:34:02 +0700

    After Lunch 2

--- app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/MyAlert.java
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/MyAlert.java
@@ -1,33 +1,38 @@
 package com.hitachi_tstv.yodpanom.yaowaluk.proofdelivery;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 
 /**
- * Created by akkarati on 2016-10-11.
+ * Created by musz on 10/11/2016.
  */
 
 public class MyAlert {
-    //Explict
+    //Explicit
     private Context context;
 
-    public MyAlert(Context context){
-        this.context =context;
+    public MyAlert(Context context) {
+        this.context = context;
     }
-    //Dialog for Error
+
+    // Dialog  for Error
     public void myErrorDialog(int intIcon,
                               String strTitle,
-                              String strMessage){
-        AlertDialog.Builder builder = new AlertDialog().Builder(context);
+                              String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setCancelable(false);
         builder.setIcon(intIcon);
         builder.setTitle(strTitle);
         builder.setMessage(strMessage);
-        builder.setPositiveButton("OK",new DialogInterface.OnClickListener(){
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view){
-                userString = userEdi
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
             }
-        })
+        });
+        builder.show();
     }
-}
+
+}   // Main Class
+
